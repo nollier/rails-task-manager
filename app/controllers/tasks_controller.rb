02e88cfb6fae6043997bd3417f3b572111c4b98e
Nicolas Ollier <nicolas-ollier@hotmail.fr>
2020-05-12 14:50:01 +0200

    add link button

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -27,15 +27,16 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
   private
 
   def task_params
     params.require(:task).permit(:title, :details)
   end
 
-  def destroy
-    @task = Task.new(find(params[:id]))
-    @task.destroy
-    redirect_to task_path
-  end
 end
